4315ceda396115023b8ac8b3288838d6b9455e20
Xyness <72358367+Xyness@users.noreply.github.com>
2024-06-18 19:51:18 +0200

    Add files via upload

--- src/main/java/fr/xyness/SCS/SimpleClaimSystem.java
+++ src/main/java/fr/xyness/SCS/SimpleClaimSystem.java
@@ -54,7 +54,7 @@ public class SimpleClaimSystem extends JavaPlugin {
 	
 	
 	public static JavaPlugin plugin;
-	public static String Version = "1.9.0.1b6";
+	public static String Version = "1.9.0.1b7";
 	public static HikariDataSource dataSource;
 	private static boolean isFolia = false;
 	private static boolean isUpdateAvailable;
