23e69a58d8043afedf2c8b32ea0c638cb8f2fb3f
Xyness <cmfarfait@icloud.com>
2024-08-18 14:33:27 +0200

    1.11.3.1

--- src/main/java/fr/xyness/SCS/SimpleClaimSystem.java
+++ src/main/java/fr/xyness/SCS/SimpleClaimSystem.java
@@ -104,7 +104,7 @@ public class SimpleClaimSystem extends JavaPlugin {
     private SimpleClaimSystem instance;
     
     /** The version of the plugin */
-    private String Version = "1.11.3";
+    private String Version = "1.11.3.1";
     
     /** Data source for database connections */
     private HikariDataSource dataSource;
